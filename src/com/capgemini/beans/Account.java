9223c96479e847060a5414dfe659bfacdace8326
Gupta <animesh.gupta@capgemini.com>
2017-04-19 12:17:02 +0530

    BankApp

---
+++ src/com/capgemini/beans/Account.java
@@ -0,0 +1,19 @@
+package com.capgemini.beans;
+
+public class Account {
+	int accountNumber;
+	int amount;
+	public int getAccountNumber() {
+		return accountNumber;
+	}
+	public void setAccountNumber(int accountNumber) {
+		this.accountNumber = accountNumber;
+	}
+	public int getAmout() {
+		return amount;
+	}
+	public void setAmout(int amount) {
+		this.amount = amount;
+	}
+
+}
